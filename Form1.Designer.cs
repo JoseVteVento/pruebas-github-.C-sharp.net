11f107ab273e32c2b65a286510fc3a8791e19ba1
Jose Vte Vento <jvvento@gmail.com>
2015-06-01 16:28:48 +0200

    Primer commit a git desde entorno .net

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Pruebas_github
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_lanzar_mensaje = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_lanzar_mensaje
+            // 
+            this.btn_lanzar_mensaje.Location = new System.Drawing.Point(91, 103);
+            this.btn_lanzar_mensaje.Name = "btn_lanzar_mensaje";
+            this.btn_lanzar_mensaje.Size = new System.Drawing.Size(104, 37);
+            this.btn_lanzar_mensaje.TabIndex = 0;
+            this.btn_lanzar_mensaje.Text = "Mostrar mensaje";
+            this.btn_lanzar_mensaje.UseVisualStyleBackColor = true;
+            this.btn_lanzar_mensaje.Click += new System.EventHandler(this.btn_lanzar_mensaje_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_lanzar_mensaje);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_lanzar_mensaje;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pruebas_github
+{
+    public partial class Form1 : Form
+    {
+        private int x = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_lanzar_mensaje_Click(object sender, EventArgs e)
+        {
+            switch (x)
+            {
+                case 0:
+                    MessageBox.Show("Primer mensaje");
+                    x++;
+                    break;
+                case 1:
+                    MessageBox.Show("Segundo mensaje");
+                    x++;
+                    break;
+                case 2:
+                    MessageBox.Show("Tercer mensaje");
+                    x++;
+                    break;
+                default:
+                    MessageBox.Show("No me quedan más mensajes. Tendremos que volver a empezar!!!");
+                    x = 0;
+                    break;
+            }     
+        }
+    }
+}
