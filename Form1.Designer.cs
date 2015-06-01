c99ede3460dc5126e7915e6885cb8e65c353a8d9
Jose Vte Vento <jvvento@gmail.com>
2015-06-01 16:51:42 +0200

    Segundo commit
    Se añade la funcionalidad de poder modificar los mensajes que aparecen en pantalla

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,14 @@
         private void InitializeComponent()
         {
             this.btn_lanzar_mensaje = new System.Windows.Forms.Button();
+            this.btn_cambiar_mensaje = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_lanzar_mensaje
             // 
-            this.btn_lanzar_mensaje.Location = new System.Drawing.Point(91, 103);
+            this.btn_lanzar_mensaje.Location = new System.Drawing.Point(89, 32);
             this.btn_lanzar_mensaje.Name = "btn_lanzar_mensaje";
             this.btn_lanzar_mensaje.Size = new System.Drawing.Size(104, 37);
             this.btn_lanzar_mensaje.TabIndex = 0;
@@ -41,21 +44,56 @@
             this.btn_lanzar_mensaje.UseVisualStyleBackColor = true;
             this.btn_lanzar_mensaje.Click += new System.EventHandler(this.btn_lanzar_mensaje_Click);
             // 
+            // btn_cambiar_mensaje
+            // 
+            this.btn_cambiar_mensaje.Location = new System.Drawing.Point(89, 101);
+            this.btn_cambiar_mensaje.Name = "btn_cambiar_mensaje";
+            this.btn_cambiar_mensaje.Size = new System.Drawing.Size(104, 37);
+            this.btn_cambiar_mensaje.TabIndex = 1;
+            this.btn_cambiar_mensaje.Text = "Cambiar mensaje";
+            this.btn_cambiar_mensaje.UseVisualStyleBackColor = true;
+            this.btn_cambiar_mensaje.Click += new System.EventHandler(this.btn_cambiar_mensaje_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 182);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nuevo mesaje:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_cambiar_mensaje);
             this.Controls.Add(this.btn_lanzar_mensaje);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Mensajes";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btn_lanzar_mensaje;
+        private System.Windows.Forms.Button btn_cambiar_mensaje;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,10 @@ namespace Pruebas_github
     public partial class Form1 : Form
     {
         private int x = 0;
+        private String mensaje1 = "Primer mensaje";
+        private String mensaje2 = "Segundo mensaje";
+        private String mensaje3 = "Tercer mensaje";
+
         public Form1()
         {
             InitializeComponent();
@@ -20,25 +24,58 @@ namespace Pruebas_github
 
         private void btn_lanzar_mensaje_Click(object sender, EventArgs e)
         {
+            label1.Visible = false;
+            textBox1.Visible = false;
+
             switch (x)
             {
                 case 0:
-                    MessageBox.Show("Primer mensaje");
+                    MessageBox.Show(mensaje1);
                     x++;
                     break;
                 case 1:
-                    MessageBox.Show("Segundo mensaje");
+                    MessageBox.Show(mensaje2);
                     x++;
                     break;
                 case 2:
-                    MessageBox.Show("Tercer mensaje");
+                    MessageBox.Show(mensaje3);
                     x++;
                     break;
                 default:
                     MessageBox.Show("No me quedan más mensajes. Tendremos que volver a empezar!!!");
                     x = 0;
                     break;
-            }     
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            textBox1.Visible = false;
+        }
+
+        private void btn_cambiar_mensaje_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            textBox1.Visible = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            switch (x)
+            {
+                case 0:
+                    mensaje1 = textBox1.Text;
+                    break;
+
+                case 1:
+                    mensaje2 = textBox1.Text;
+                    break;
+
+                case 2:
+                    mensaje3 = textBox1.Text;
+                    break;
+            }
         }
     }
 }
